bcf7d27ad67e9c5c78f082cf1f9b9d3c1524e4f7
dz2 <dz2@ualberta.ca>
2017-10-23 13:46:33 -0600

    unitTest6

---
+++ app/src/androidTest/java/dizhang/com/example/tiramisu/ShareActivityTest.java
@@ -0,0 +1,69 @@
+package dizhang.com.example.tiramisu;
+
+import android.app.Activity;
+import android.test.ActivityInstrumentationTestCase2;
+import android.util.Log;
+import android.widget.EditText;
+
+import com.robotium.solo.Solo;
+
+import org.junit.After;
+import org.junit.Before;
+
+/**
+ * Created by ggranked on 2017-10-23.
+ */
+
+public class ShareActivityTest extends ActivityInstrumentationTestCase2<ShareActivity>
+
+{
+    private Solo solo;
+
+    public ShareActivityTest() {
+        super(dizhang.com.example.tiramisu.ShareActivity.class);
+    }
+
+
+    @Before
+    public void setUp() throws Exception {
+        solo = new Solo(getInstrumentation(), getActivity());
+        Log.d("Setup", "setUp()");
+    }
+
+
+    public void testStart() throws Exception {
+        Activity activity = getActivity();
+    }
+
+    public void testClickOnButton() {
+        ShareActivity activity = (ShareActivity) solo.getCurrentActivity();
+
+        solo.assertCurrentActivity("Wrong Activity", ShareActivity.class);
+
+        solo.clickInList(0);
+        solo.assertCurrentActivity("Wrong Activity", FollowedActivity.class);
+        solo.waitForText("Test follow");
+
+        solo.goBack();
+        solo.assertCurrentActivity("Wrong Activity", ShareActivity.class);
+
+        solo.enterText((EditText) solo.getView(R.id.editText), "TestUsername");
+        solo.clickOnButton("Sent a Request");
+
+        solo.clickOnButton("Check Request From Others");
+        solo.assertCurrentActivity("Wrong Activity", RequestActivity.class);
+
+        solo.clickInList(0, 1);
+        solo.clickInList(0, 2);
+        solo.goBack();
+
+        solo.assertCurrentActivity("Wrong Activity", ShareActivity.class);
+
+    }
+
+
+    @After
+    public void tearDown() throws Exception {
+        solo.finishOpenedActivities();
+    }
+}
