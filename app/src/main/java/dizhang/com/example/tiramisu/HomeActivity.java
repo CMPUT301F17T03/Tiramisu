43823b153b38ec6282b5fe0f205e8ba95a14de6c
dz2 <dz2@ualberta.ca>
2017-10-22 19:31:38 -0600

    NavigationUpdate2

---
+++ app/src/main/java/dizhang/com/example/tiramisu/HomeActivity.java
@@ -0,0 +1,41 @@
+package dizhang.com.example.tiramisu;
+
+import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+
+/**
+ * Created by ggranked on 2017-10-22.
+ */
+
+public class HomeActivity extends AppCompatActivity {
+
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mtoggle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        mtoggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.open, R.string.close);
+
+        mDrawerLayout.addDrawerListener(mtoggle);
+        mtoggle.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(mtoggle.onOptionsItemSelected(item)){
+            return true;
+
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
--- app/src/main/java/dizhang/com/example/tiramisu/LoginActivity.java
+++ app/src/main/java/dizhang/com/example/tiramisu/LoginActivity.java
@@ -1,13 +1,43 @@
 package dizhang.com.example.tiramisu;
 
+import android.app.ActionBar;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.support.design.widget.NavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity{
+
+    public TextView signupButton;
+    public RelativeLayout loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        //ActionBar bar = getActionBar();
+        //bar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#00C4CD")));
+
+        signupButton = (TextView) findViewById(R.id.signupLayout);
+        loginButton = (RelativeLayout) findViewById(R.id.loginLayout);
+
+
+
+        loginButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
+
+
 }
