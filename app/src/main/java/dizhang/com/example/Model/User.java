eedebd89441cdaf6fb065546af31f886f88796dc
dz2 <dz2@ualberta.ca>
2017-11-06 18:47:55 -0700

    ModelUpdate2

--- app/src/main/java/dizhang/com/example/Model/User.java
+++ app/src/main/java/dizhang/com/example/Model/User.java
@@ -1,17 +1,26 @@
 package dizhang.com.example.Model;
 
+import android.graphics.Picture;
+
 /**
  * Created by ggranked on 2017-11-06.
  */
 
 public class User {
     private String username; //unique ID
+    private String nickname;
+    private String gender;
+    private String interests;
+    private Picture profilePic;
     private HabitList habitlist;
     private EventList eventlist;
     private HistoryList history;
     private UserList fans = new UserList();     //users who follows me
     private UserList requests = new UserList(); //requests sent to me
     private UserList following = new UserList(); //the users that I followed
+
+    //TODO: generate getter and setter
+
 
 
 
--- app/src/main/java/dizhang/com/example/Model/UserList.java
+++ app/src/main/java/dizhang/com/example/Model/UserList.java
@@ -1,8 +1,29 @@
 package dizhang.com.example.Model;
 
+import java.util.ArrayList;
+
 /**
  * Created by ggranked on 2017-11-06.
  */
 
 public class UserList {
+    private ArrayList<User> users = new ArrayList<User>();
+
+    public UserList(){}
+
+    public User getuser(int index) {
+        return users.get(index);
+    }
+
+    public boolean hasuser (User user){
+        return users.contains(user);
+    }
+
+    public void add(User user){
+        users.add(user);
+    }
+
+    public void delete(User user) {
+        users.remove(user);
+    }
 }
