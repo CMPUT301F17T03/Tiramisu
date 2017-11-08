ccc7708e8d027eda2d46533664ea5507ee52cac7
dz2 <dz2@ualberta.ca>
2017-11-07 18:58:17 -0700

    datePickerUpdate1

--- app/src/main/java/dizhang/com/example/Control/HabitNewActivity.java
+++ app/src/main/java/dizhang/com/example/Control/HabitNewActivity.java
@@ -1,15 +1,57 @@
 package dizhang.com.example.Control;
 
+import android.app.DatePickerDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+import java.util.Calendar;
 
 import dizhang.com.example.tiramisu.R;
 
-public class HabitNewActivity extends AppCompatActivity {
+public class HabitNewActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener{
+
+    Button startDate;
+    EditText newTitle, newDes;
+    int day, month, year;
+    int dayFinal, monthFinal, yearFinal;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_habit_new);
+
+        startDate = (Button) findViewById(R.id.startDate);
+        newTitle = (EditText) findViewById(R.id.newTitle);
+        newDes = (EditText) findViewById(R.id.newDes);
+
+        startDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Calendar c = Calendar.getInstance();
+                year = c.get(Calendar.YEAR);
+                month = c.get(Calendar.MONTH);
+                day = c.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog datePickerDialog = new DatePickerDialog(HabitNewActivity.this, HabitNewActivity.this,
+                        year, month, day);
+                datePickerDialog.show();
+
+            }
+        });
+
+    }
+
+    @Override
+    public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
+        yearFinal = i;
+        monthFinal = i1+1;
+        dayFinal = i2;
+        
+
     }
 }
--- app/src/main/java/dizhang/com/example/View/HabitViewActivity.java
+++ app/src/main/java/dizhang/com/example/View/HabitViewActivity.java
@@ -1,15 +1,32 @@
 package dizhang.com.example.View;
 
+import android.content.Intent;
+import android.provider.BlockedNumberContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+import dizhang.com.example.Control.HabitNewActivity;
+import dizhang.com.example.Model.Habit;
 import dizhang.com.example.tiramisu.R;
 
 public class HabitViewActivity extends AppCompatActivity {
+    Button createNew;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_habit_view);
+
+        createNew = (Button) findViewById(R.id.createNew);
+
+        createNew.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent newHabitInt = new Intent(getApplicationContext(), HabitNewActivity.class);
+                startActivity(newHabitInt);
+            }
+        });
     }
 }
